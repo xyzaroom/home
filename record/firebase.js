1e63ed276731182d6af84201d54de61c373f4546
Xyzaroom <234803722+xyzaroom@users.noreply.github.com>
2025-11-09 14:20:06 +0700

    record

---
+++ record/firebase.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAkCFiep7aZf7pVZXTn2VoCADaIAl4xep8",
+  authDomain: "xyzaroom-597c4.firebaseapp.com",
+  projectId: "xyzaroom-597c4",
+  storageBucket: "xyzaroom-597c4.firebasestorage.app",
+  messagingSenderId: "927885453365",
+  appId: "1:927885453365:web:7cb877d61902e9504ce97c",
+  measurementId: "G-6VWERM6B9Z"
+};
